1c92075d38751d9e69f4408d5861bc1f13fcf110
Felipe Ornellas <felipe10ornellas@gmail.com>
2025-12-09 20:32:28 -0300

    Initial commit

---
+++ src/main/java/Rinamed/sistema_comercial/ClienteRepository.java
@@ -0,0 +1,7 @@
+package Rinamed.sistema_comercial;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+}
---
+++ src/main/java/Rinamed/sistema_comercial/SistemaComercialApplication.java
@@ -0,0 +1,13 @@
+package Rinamed.sistema_comercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaComercialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaComercialApplication.class, args);
+	}
+
+}
